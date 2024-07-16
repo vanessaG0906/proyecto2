061ef8095ef348089f8f90f8035cc97e859cbd5d
Gómez Palma Dania Vanessa <134179962+vanessaG0906@users.noreply.github.com>
2024-07-16 00:24:28 -0500

    Add files via upload

--- espacioNombres.php
+++ espacioNombres.php
@@ -4,17 +4,37 @@
 	<title>SimpleXML | Espacio de nombres</title>
 	<meta charset="utf-8">
 	<?php
+	libxml_use_internal_errors(true);
 	$libros_xml = simplexml_load_file("libros.xml");
-	$doc_ns = $libros_xml->libro[0]->getDocNamespaces();
-	$ns = $libros_xml->libro[0]->autor->getNamespaces(true);
-
-	print "<pre>";
-	print var_dump($doc_ns);
-	print "</pre>";
-	print "<hr>";
-	print "<pre>";
-	print var_dump($ns);
-	print "</pre>";
+	if ($libros_xml===false) {
+		foreach (libxml_get_errors() as $error) {
+			print "Nivel: ".$error->level."<br>";
+			print "Codigo: ".$error->code."<br>" ;
+			print "Columna: ".$error->column."<br>" ;
+			print "Mensaje: ".$error->message."<br>" ;
+			print "Archivo: ".$error->file."<br>" ;
+			print "Linea: ".$error->line."<br>" ;
+			print "<hr>";
+		}
+	} else {
+		print "<pre>";
+		print var_dump($libros_xml);
+		print "</pre>";
+		/*
+		print "Total de libros: ".$libros_xml->count()."<hr>";
+		foreach ($libros_xml as $libro) {
+				print strtoupper($libro->titulo)."<br>";
+				print "Páginas: ".$libro->titulo["paginas"]."<br>";
+				print "Autor: ".$libro->autor->{"nombre"}." ";
+				print $libro->autor->{"apellido-paterno"}." ";
+				print $libro->autor->{"apellido.materno"}."<br>";
+				print "Editorial: ".$libro->editorial."<br>";
+				print "Precio: $".$libro->precio."<br>";
+				print "Fecha: ".$libro->{"año"}."<br>";
+				print "<hr>";
+		}
+		*/
+	}
 	?>
 </head>
 <body>
